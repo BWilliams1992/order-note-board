cf74bca008dee0af8258a15b599904c7c481a82d
Ben Williams <ben.williams@weareaccess.co.uk>
2026-05-13 14:45:12 +0100

    Initialise vue app

---
+++ resources/js/Pages/Notes/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2 class="text-3xl font-bold text-center my-8 py-4">Notes</h2>
+    <div class="flex flex-col overflow-x-auto">
+
+    </div>
+</template>
+
+<script setup>
+
+defineProps({
+  notes: Object,
+})
+
+</script>
