9964f1334a6bceb4896135754453d120890600a1
Ben Williams <ben.williams@weareaccess.co.uk>
2026-05-13 15:31:40 +0100

    Display notes on front end

---
+++ resources/js/Components/Notes/NoteCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      Author: {{ note.author }}
+    </div>
+    <div>
+      Order number:{{ note.order_number }}
+    </div>
+    <div>
+      Message: {{ note.message }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    note: Object,
+  })
+</script>
--- resources/js/Pages/Notes/Index.vue
+++ resources/js/Pages/Notes/Index.vue
@@ -3,6 +3,12 @@
       <h1 class="text-3xl font-bold text-center my-4 py-4">Notes</h1>
       <div class="flex flex-col overflow-x-auto">
         <CreateForm/>
+        <h2 class="font-semibold text-lg">Notes listing:</h2>
+        <div v-if="notes">
+          <div v-for="note in notes">
+            <NoteCard :note="note"/>
+          </div>
+        </div>
       </div>
     </div>
 </template>
@@ -10,6 +16,7 @@
 <script setup>
   import { usePage } from '@inertiajs/vue3';
   import CreateForm from '../../Components/Notes/CreateForm.vue';
+import NoteCard from '../../Components/Notes/NoteCard.vue';
 
   const page = usePage();
 
