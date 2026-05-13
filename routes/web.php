55444141c7c71a7e92da9f57ad7e41a65478b640
Ben Williams <ben.williams@weareaccess.co.uk>
2026-05-13 14:44:59 +0100

    Update base route to point to notes index

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [ NoteController::class, 'index']);
+
 Route::post('/api/notes', [ NoteController::class, 'store']);
